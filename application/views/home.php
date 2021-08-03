945a034cb6ccedf6719a378ad1089fbb50b1aea9
sayan <sayanmandal8643@gmail.com>
2021-08-03 18:49:10 +0530

    initial commit

---
+++ application/views/home.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<div class="container">
+    <div class="class row">
+        <div class="col-lg-4 col-md-4 col-lg-oush-4 col-md-push-4">
+            <div class="panel panel-default" style="margin-top: 50px">
+                <div class="panel-heading">Login</div>
+                <div class="panel-body">
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
